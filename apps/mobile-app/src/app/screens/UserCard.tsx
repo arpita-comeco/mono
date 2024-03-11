2eadc8cc4741f0c923d701d71adfce6b03d8dd55
Arpita Behura <98814410+arpita-comeco@users.noreply.github.com>
2024-03-11 11:33:30 +0100

    Added a new component UserCard to print the user details in card style for React Native app

---
+++ apps/mobile-app/src/app/screens/UserCard.tsx
@@ -0,0 +1,54 @@
+// UserCard.tsx
+
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import {User} from "@common/utils/types/user";
+
+interface UserCardProps {
+  user: User;
+}
+
+const UserCard: React.FC<UserCardProps> = ({ user }) => {
+  return (
+    <View style={styles.card}>
+      <Image source={{ uri: user.image }} style={styles.image} />
+      <View style={styles.userInfo}>
+        <Text style={styles.userName}>{user.firstName} {user.lastName}</Text>
+        <Text style={styles.userDetails}>{user.email}</Text>
+        <Text style={styles.userDetails}>{user.phone}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    padding: 20,
+    marginBottom: 10,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 20,
+  },
+  userInfo: {
+    flex: 1,
+  },
+  userName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  userDetails: {
+    fontSize: 16
+  }
+});
+
+export default UserCard;
